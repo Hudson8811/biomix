efe158d1dd8796c9d2f889c76f652e99763d8bb1
PipZip <zipperpipper@gmail.com>
2020-08-13 20:48:52 +0600

    gulp

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+//=require './jquery-3.5.1.min.js'
+//=require './slick.js'
+//=require './select2.full.min.js'
+//=require './autosize.min.js'
+
+//=require './fullpage.js'
+
+
+$(document).ready(function () {
+
+
+});
