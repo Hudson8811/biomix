09c1cb1b136789ab8ce37ba5fa7a674d9ab8633c
ArtZ <snickgem@gmail.com>
2020-08-27 19:03:27 +0400

    fix

--- src/js/partials/send.js
+++ src/js/partials/send.js
@@ -3,20 +3,6 @@ function send(event, php) {
 	event.preventDefault ? event.preventDefault() : event.returnValue = false;
 	var req = new XMLHttpRequest();
 	req.open('POST', php, true);
-	req.onload = function () {
-		if (req.status >= 200 && req.status < 400) {
-			json = JSON.parse(this.response);
-			console.log(json);
-
-			if (json.result == "success") {
-				console.log("Сообщение отправлено");
-			} else {
-				console.log("Сообщение не отправлено");
-			}
-		} else {
-			console.log("Ошибка сервера " + req.status);
-		}
-	};
 
 	req.onerror = function () {
 		console.log("Ошибка отправки запроса");
